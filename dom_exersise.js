b843bad2b2cb7d496bfa51d55974749f9912a42c
PonomarenkoNina <ninulka@ukr.net>
2024-01-18 23:20:31 +0200

    added two files

---
+++ dom_exersise.js
@@ -0,0 +1,5 @@
+document.getElementById("greenplanet");
+var planet = document.getElementById("greenplanet");
+planet.innerHTML = "Red Alert: hit by phaser fire!";
+console.log(planet.innerHTML);
+planet.innerHTML = "Red Alert: hit by phaser fire!";
