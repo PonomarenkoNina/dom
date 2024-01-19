ce4d3e78886a2b41155c40fc52c6553000554ad3
PonomarenkoNina <ninulka@ukr.net>
2024-01-19 23:05:24 +0200

    script moved to head and delete 1,4,5 lines removed

--- dom_exersise.js
+++ dom_exersise.js
@@ -1,5 +1,3 @@
-document.getElementById("greenplanet");
 var planet = document.getElementById("greenplanet");
 planet.innerHTML = "Red Alert: hit by phaser fire!";
-console.log(planet.innerHTML);
-planet.innerHTML = "Red Alert: hit by phaser fire!";
+// Console JS: dom_exersise.js:2 Uncaught TypeError: Cannot set properties of null (setting 'innerHTML')
